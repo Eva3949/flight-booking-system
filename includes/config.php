7ccc21e7a716140ca31562b7b687f9c50cc2c59b
Samuel Tenkir <samueleva3949@gmail.com>
2026-05-10 10:35:05 +0300

    db info change

--- includes/config.php
+++ includes/config.php
@@ -6,11 +6,16 @@
 // define('DB_NAME', 'if0_41562340_flight_booking_system');
 // define('DB_PORT', '3306');
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'flight_booking_system');
+define('DB_SERVER', 'sql300.infinityfree.com');
+define('DB_USERNAME', 'if0_41562340');
+define('DB_PASSWORD', 'Eva39499987');
+define('DB_NAME', 'if0_41562340_flight_booking_system');
 define('DB_PORT', '3306');
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'flight_booking_system');
+// define('DB_PORT', '3306');
 
 // Application configuration
 define('SITE_NAME', 'Flight Booking System');
